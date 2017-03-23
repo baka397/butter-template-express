aaceafbd77625c64d29b748172e9cd1a6e8b8094
bakajinsei <cqggff@gmail.com>
2017-03-23 11:50:57 +0800

    Fixed redis Cluster log

--- template/common/redis.js
+++ template/common/redis.js
@@ -7,11 +7,21 @@ let redisClient = new Redis.Cluster(config.redisNodes);
 {{else}}
 let redisClient = new Redis(config.redis);
 {{/if}}
+{{#if redis-cluster}}
 redisClient.on('error', function (err) {
-    log.error('Redis connect error',config.redis.host+':'+config.redis.port);
+    log.error('Redis connect error:'+config.redisNodes.toString());
     log.error(err);
 });
 redisClient.on('connect', function () {
-    log.info('Redis connect succeed',config.redis.host+':'+config.redis.port);
+    log.info('Redis connect succeed:'+config.redisNodes.toString());
 });
+{{else}}
+redisClient.on('error', function (err) {
+    log.error('Redis connect error:'+config.redis.host+':'+config.redis.port);
+    log.error(err);
+});
+redisClient.on('connect', function () {
+    log.info('Redis connect succeed:'+config.redis.host+':'+config.redis.port);
+});
+{{/if}}
 module.exports = redisClient;
