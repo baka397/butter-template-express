414f6bca84446d11c7dcd8b73cb2eae540fedeca
bakajinsei <cqggff@gmail.com>
2017-03-27 17:48:29 +0800

    Fixed gulp watch bug

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -3,26 +3,30 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const minifycss = require('gulp-minify-css');
 const del = require('del');
-{{#if less}}
 const less = require('gulp-less');
-{{/if}}
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify'); //显示报错信息和报错后不终止当前gulp任务
+const plumber = require('gulp-plumber'); //捕获处理任务中的错误
 
 /**
  * 清除
  */
 gulp.task('clean', function(cb) {
-    del(['*.zip','public','views','build/css','logs'], cb);
+    del(['*.zip', 'public', 'views', 'build/css', 'logs'], cb);
 });
-{{#if less}}
 gulp.task('less', function(cb) {
-    gulp.src('./build/less/*.less').pipe(less()).pipe(gulp.dest('./build/css')).on('end', cb); //编译less
+    //编译less
+    gulp.src('./build/less/*.less')
+        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+        .pipe(less())
+        .pipe(gulp.dest('./build/css'))
+        .on('end', cb);
 });
-{{/if}}
 
 /**
  * 开发默认
  */
-gulp.task('default',[{{#if less}}'less'{{/if}}],function() {
+gulp.task('default', ['less'], function() {
     gulp.src(['./build/img/**']).pipe(gulp.dest('./public/img'));
     gulp.src(['./build/css/**']).pipe(gulp.dest('./public/css'));
     gulp.src(['./build/fonts/**']).pipe(gulp.dest('./public/fonts'));
@@ -34,8 +38,14 @@ gulp.task('default',[{{#if less}}'less'{{/if}}],function() {
 /**
  * 上线编译任务
  */
-gulp.task('build',[{{#if less}}'less'{{/if}}],function() {
+gulp.task('build', ['less'], function() {
     gulp.src(['./build/img/**']).pipe(gulp.dest('./public/img'));
+    gulp.src(['./public/img/**/*.{png,jpg,jpeg,gif,ico}']).pipe(imagemin({
+        optimizationLevel: 7, //默认：3  取值范围：0-7（优化等级）
+        progressive: true, //默认：false 无损压缩jpg图片
+        interlaced: true, //默认：false 隔行扫描gif进行渲染
+        multipass: true //默认：false 多次优化svg直到完全优化
+    })).pipe(gulp.dest('./public/img')); //压缩图片
     gulp.src(['./build/css/*.css']).pipe(minifycss()).pipe(gulp.dest('./public/css')); //压缩工程输出工程
     gulp.src(['./build/css/plugins/**']).pipe(gulp.dest('./public/css/plugins'));//打包插件
     gulp.src(['./build/fonts/**']).pipe(gulp.dest('./public/fonts'));
@@ -44,10 +54,10 @@ gulp.task('build',[{{#if less}}'less'{{/if}}],function() {
     gulp.src(['./build/data/**']).pipe(gulp.dest('./public/data'));
     gulp.src(['./build/views/**']).pipe(gulp.dest('views'));
 });
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch('build/**', ['default']);
 });
 
-gulp.task('buildwatch', function () {
+gulp.task('buildwatch', function() {
     gulp.watch('build/**', ['build']);
 });
